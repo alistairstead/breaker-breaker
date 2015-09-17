ae16e493d8fe48f71250adc9e8ddd9344f4046d9
Alistair Stead <alistair.stead@designdisclosure.com>
2015-09-17 08:18:54 +0100

    Extract the command creation to a new file

---
+++ test/commandSpec.js
@@ -0,0 +1,51 @@
+/* global describe, it */
+'use strict';
+
+import chai from 'chai';
+chai.should();
+
+import { Command } from '../lib/command';
+import { CallbackTarget } from './fixtures/callback';
+
+describe.only('Command', function () {
+  let callback = new CallbackTarget();
+
+  it('should wrap the supplied callable in a promise', function () {
+    Command.create(function () { return 'result'; }, undefined, []).then.should.be.a('function');
+  });
+
+  it('should resolve with the result of the supplied syncronous call', function (done) {
+    Command.create(function () { return 'result'; }, undefined, []).then(function (result) {
+      result.should.equal('result');
+      done();
+    });
+  });
+
+  it('should respect the callback API for success', function (done) {
+    Command.create(callback.methodWillSucceed, undefined, [function (err, result) {
+      if (err) {
+        throw err;
+      }
+      result.should.equal('Success');
+      done();
+    }]);
+  });
+
+  it('should respect the callback API for errors', function (done) {
+    Command.create(callback.methodWillFail, undefined, [function (err, result) {
+      err.message.should.equal('Method failed');
+      done();
+    }]);
+  });
+
+  it('should allow the use of the promise API for a callback success', function (done) {
+    Command.create(callback.methodWillSucceed, undefined, [function () {}]).then(function (result) {
+      result.should.equal('Success');
+      done();
+    });
+  });
+
+  it('should simply return an exiting promise untouched', function () {
+    Command.create(new Promise(function (resolve, reject) {resolve('Success');})).then.should.be.a('function');
+  });
+});
